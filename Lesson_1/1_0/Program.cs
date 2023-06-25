5e0122f6c674dc755ec8fb552c0083759b03cb8c
sergey <kozhokar.s@mail.ru>
2023-06-25 11:56:14 +0300

    Classwork 1_0

---
+++ Lesson_1/1_0/Program.cs
@@ -0,0 +1,8 @@
+﻿// напишите программу, которая принимает число и выдает уго квадрат (число умноженое на само себя)
+
+Console.WriteLine("Write a number: ");
+string num_str = Console.ReadLine()!;
+
+int num = int.Parse(num_str);
+
+Console.WriteLine(num * num);
