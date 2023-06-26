778e29dfc32004f0701499c7dd73404e529621cf
sergey <kozhokar.s@mail.ru>
2023-06-26 21:06:12 +0300

    lesson2

---
+++ Lesson_2/2_1/Program.cs
@@ -0,0 +1,14 @@
+﻿int Max(int num)
+{
+    if (num/10 > num % 10 )
+    return num/10;
+
+    return num % 10;
+    
+}
+int RNum = new Random().Next (10,100);
+Console.WriteLine("" + RNum);
+int result = Max(RNum);
+Console.WriteLine("" + result);
+
+
