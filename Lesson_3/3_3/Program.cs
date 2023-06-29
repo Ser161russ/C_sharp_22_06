b1719264de48fab118020c765feef2ff9afaf3fa
sergey <kozhokar.s@mail.ru>
2023-06-29 22:16:43 +0300

    lesson_3

---
+++ Lesson_3/3_3/Program.cs
@@ -0,0 +1,27 @@
+﻿// 3. Напишите программу, которая принимает на вход число (N) 
+//    и выдаёт таблицу квадратов чисел от 1 до N.
+
+
+ int a = int.Parse(Console.ReadLine()!);
+int count = 1;
+if (a == 0 )
+{
+ Console.WriteLine("1");
+}
+else if ( a > 0 )
+{
+    while (Math.Pow(count,2) <= a)
+    {
+        Console.WriteLine(Math.Pow(count, 2));
+        count++;
+    }
+}
+else if ( a < 0 )
+    {
+        while (Math.Pow(count,2) >= a )
+        {
+            Console.WriteLine(Math.Pow(count, 2));
+             count--;
+        }
+
+    }
