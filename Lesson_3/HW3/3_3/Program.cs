55ea6abbc8d15250c8336cd1349de76d974da7b1
sergey <kozhokar.s@mail.ru>
2023-07-03 23:08:24 +0300

    добавил HW3_3

---
+++ Lesson_3/HW3/3_3/Program.cs
@@ -0,0 +1,19 @@
+﻿// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+//3 -> 1, 8, 27
+//5 -> 1, 8, 27, 64, 125
+Console.WriteLine("Введите число произведений в степень");
+int N = int.Parse(Console.ReadLine()!);
+int I = 1;
+double result = 1;
+
+if (N == 0)
+Console.WriteLine("значение равно 1");
+else do
+{
+
+result = Math.Pow(I, 3);
+Console.WriteLine("Значения = " + result);
+I=I+1;
+
+}
+while (I <= N);
