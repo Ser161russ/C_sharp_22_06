9cbdbde4ec460d6ab6448ee49f82c9a54fdb02db
sergey <kozhokar.s@mail.ru>
2023-07-12 21:35:58 +0300

    lesson 3

--- Lesson_3/HW3/3_1/Program.cs
+++ Lesson_3/HW3/3_1/Program.cs
@@ -1,1 +1,24 @@
-﻿ 
+﻿//  Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+// 14212 -> нет
+// 12821 -> да
+// 23432 -> да
+ 
+ 
+ 
+
+ void Pali(int num)
+ {
+      int num1 =num / 1000;
+      int num2 =num % 10;
+      int num3 =num / 10 % 10;
+
+   if (num1 == num2 * 10 + num3)
+   Console.WriteLine($"Да,{num}");
+
+   else 
+   Console.WriteLine($"Нет,{num}");
+}
+ 
+Console.WriteLine("Введите 5 цифр");
+int val = int.Parse(Console.ReadLine()!);
+Pali(val);
